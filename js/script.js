4282fc396c28d5d33e170d1c2d5a05b0bf6fcd3f
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-28 15:31:34 +0100

    (js/previousSlide) add new function to turn back in slides and add in html

--- js/script.js
+++ js/script.js
@@ -13,12 +13,20 @@ createApp({
         // prima funzione da associare al pulsante per andare avanti sulle slide
         nextSlide(){
             if(this.firstSlideActive < this.slides.length - 1){ // se l'indice della slide attiva è inferiore al numero di slide - 1
-            this.firstSlideActive++; // incrementa l'indice della slide attiva
+                this.firstSlideActive++; // incrementa l'indice della slide attiva
             } else { // altrimenti
                 this.firstSlideActive = 0; // imposta l'indice della slide attiva a 0
             }
         },
+        previousSlide(){
+            if(this.firstSlideActive > 0){// se l'indice della slide attiva è superiore a 0
+                this.firstSlideActive--; // decrementa l'indice della slide attiva
+            } else { // altrimenti
+                this.firstSlideActive = this.slides.length - 1; // imposta l'indice della slide attiva al numero di slide - 1
+            }
+        }
     },
+
     mounted() {
 
     }
