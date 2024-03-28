93c38cafbc86c403dbc981579ee5ceede82ccfdf
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-28 15:38:31 +0100

    (js/ goToSlide ) insert a function to switch slide with a click on it

--- js/script.js
+++ js/script.js
@@ -18,12 +18,17 @@ createApp({
                 this.firstSlideActive = 0; // imposta l'indice della slide attiva a 0
             }
         },
+        // seconda funzione da associare al pulsante per tornare indietro sulle slide
         previousSlide(){
             if(this.firstSlideActive > 0){// se l'indice della slide attiva è superiore a 0
                 this.firstSlideActive--; // decrementa l'indice della slide attiva
             } else { // altrimenti
                 this.firstSlideActive = this.slides.length - 1; // imposta l'indice della slide attiva al numero di slide - 1
             }
+        },
+        // aggiungo una funzione che mi permetta di cambiare tramite un evento non ancora specificato la slide attiva
+        goToSlide(i){ // funzione con argomanto i (l'indice)
+            this.firstSlideActive = i; // imposta l'indice della slide attiva
         }
     },
 
