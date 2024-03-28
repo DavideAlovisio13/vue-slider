849af792d0f0dc2e75534b3888b6b036d000312d
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-28 15:01:38 +0100

    (index/.slider-wrapper) insert first img index 0

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 import { slides } from './data.js';
 
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            slides: slides, // inseriamo un osservatore delle interazioni delle interazioni dell'oggetto in questione
+            firstSlideActive: 0 // inizializziamo l'indice della slide attiva, che inizialmente sarà la prima
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+
+    }
+    
+}).mount('#app');
